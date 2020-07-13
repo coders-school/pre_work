7143dbb055639791b90837983ea3d326d2989229
Marcin Koczwara <marcin.koczwara@consult.red>
2020-07-13 13:28:21 +0200

    Fixed typo in Prime number test, created sumNumbersFromGenerators function.

--- src/UtilityFunctions.cpp
+++ src/UtilityFunctions.cpp
@@ -41,10 +41,12 @@ int sumGeneratedNumbers(std::unique_ptr<NumberGenerator> gen, int num)
 
 int sumNumbersFromGenerators(std::vector<GeneratorType> const & types, int num)
 {
-    // TODO: Write a loop that goes over a collection of GeneratorType values (types)
-    // and creates generator for given type (use createGenerator function),
-    // then sums up all generated values for each type and returns the sum of sums.
-    // Use sumGeneratedNumbers to sum generated values for each generator.
-    return 42;
+    int sum = 0;
+
+    for (auto const& type:types)
+    {
+        sum += sumGeneratedNumbers(createGenerator(type), num);
+    }
+    return sum;
 }
 
