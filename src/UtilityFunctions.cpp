1c6af4d0465e2a65659c38053c06ceed71f92755
Mateusz Hobgarski <mateusz.hobgarski@redembedded.com>
2020-07-13 13:58:17 +0200

    Unit tests corrected

--- src/UtilityFunctions.cpp
+++ src/UtilityFunctions.cpp
@@ -41,10 +41,12 @@ int sumGeneratedNumbers(std::unique_ptr<NumberGenerator> gen, int num)
 
 int sumNumbersFromGenerators(std::vector<GeneratorType> const & types, int num)
 {
-    // TODO: Write a loop that goes over a collection of GeneratorType values (types)
-    // and creates generator for given type (use createGenerator function),
-    // then sums up all generated values for each type and returns the sum of sums.
-    // Use sumGeneratedNumbers to sum generated values for each generator.
-    return 42;
+    int sum = 0;
+    for(int i = 0; i < types.size(); ++i)
+    {
+        auto gen = createGenerator(types[i]);
+        sum += sumGeneratedNumbers(move(gen), num);
+    }
+    return sum;
 }
 
