ee8272c1ea596d2df4a671fd556f5e9fdd18c950
Waqar Altaf <waqar.altaf@consult.red>
2020-07-14 23:14:03 +0100

    Implementation of sumNumbersFromGenerators()
    - all unit tests pass
    - environment setup with Liveshare in Vscode

--- src/UtilityFunctions.cpp
+++ src/UtilityFunctions.cpp
@@ -45,6 +45,13 @@ int sumNumbersFromGenerators(std::vector<GeneratorType> const & types, int num)
     // and creates generator for given type (use createGenerator function),
     // then sums up all generated values for each type and returns the sum of sums.
     // Use sumGeneratedNumbers to sum generated values for each generator.
-    return 42;
+
+    int sumOfSums{0};
+    for (auto entry : types)
+    {
+        sumOfSums += sumGeneratedNumbers(createGenerator(entry), num);
+    }
+
+    return sumOfSums;
 }
 
