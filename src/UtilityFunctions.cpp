9f56b169b09c930956dbe1733486f82350b4ce1e
Simon Hemming <sh0505.att.com>
2020-07-14 08:37:18 +0100

    Fixed final value for prime unit test. Implemented sumNumbersFromGenerators() function for unit test ./NumberGenerators-ut

--- src/UtilityFunctions.cpp
+++ src/UtilityFunctions.cpp
@@ -45,6 +45,12 @@ int sumNumbersFromGenerators(std::vector<GeneratorType> const & types, int num)
     // and creates generator for given type (use createGenerator function),
     // then sums up all generated values for each type and returns the sum of sums.
     // Use sumGeneratedNumbers to sum generated values for each generator.
-    return 42;
+    int total= 0;
+    for ( size_t i = 0; i < types.size(); i++) 
+    {
+        std::unique_ptr<NumberGenerator> numGenerator = createGenerator( types[ i ] );
+        total += sumGeneratedNumbers(std::move(numGenerator), num );
+    }
+    return total;
 }
 
